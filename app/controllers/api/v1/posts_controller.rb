bbb3e0ed44f33376130965e2cb028c2a4622813f
javierc22 <javier.ec.22@gmail.com>
2021-07-22 23:29:20 -0400

    Crear publicación

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,24 @@
 module Api::V1
   class PostsController < ApplicationController
     def index
-      render json: { message: "Hola" }, status: :ok
+      posts = Post.all
+      render json: posts, each_serializer: PostSerializer, status: :ok
+    end
+
+    def create
+      post = Post.new(post_params)
+
+      if post.save
+        render json: { msg: "Post created", success: true}, status: :created
+      else
+        render json: { msg: "Error", success: false}, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:title, :extract, :content)
     end
   end
 end
