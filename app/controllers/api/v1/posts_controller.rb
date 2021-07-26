c9c5073f0d3507e0a6a2f6998610bc948968b2e7
javierc22 <javier.ec.22@gmail.com>
2021-07-25 22:59:00 -0400

    Actualizar publicación

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
   class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :destroy, :update]
+
     def index
       posts = Post.all
       render json: posts, each_serializer: PostSerializer, status: :ok
@@ -9,13 +11,29 @@ module Api::V1
       post = Post.new(post_params)
 
       if post.save
-        render json: { msg: "Post created", success: true}, status: :created
+        render json: { msg: "Post created", post_id: post.id, success: true}, status: :created
+      else
+        render json: { msg: "Error", success: false }, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      render json: @post, status: :ok
+    end
+
+    def update
+      if @post.update(post_params)
+        render json: { msg: "Post updated", success: true}, status: :ok
       else
         render json: { msg: "Error", success: false}, status: :unprocessable_entity
       end
     end
 
     private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 
     def post_params
       params.require(:post).permit(:title, :extract, :content)
